a93dc2c924d98a7bad608f3f80bf96bedb18aac0
Hamidjon03 <forevalm@gmail.com>
2023-03-20 23:23:03 +0500

    getAllTasks

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('Task Manager App')
+
+const express = require("express");
+const app = express();
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json())
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Task manager is began")
+})
+
+app.use('/api/v1/taks', tasks)
+
+const port = 3000;
+app.listen(port, console.log(`Server is running on port ${port}`))
---
+++ controllers/tasks.js
@@ -0,0 +1,10 @@
+
+
+
+const getAllTasks = (req, res) => {
+  res.send('all items,items are getting by tasks controller')
+}
+
+module.exports = {
+  getAllTasks
+}
