6bb62f56f0498634a9ba0bf9e79b2856b6b6e2ac
Hamidjon03 <forevalm@gmail.com>
2023-03-20 23:47:19 +0500

    allRoutes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,29 @@
 
 
-
 const getAllTasks = (req, res) => {
   res.send('all items,items are getting by tasks controller')
 }
 
+const createTask = (req, res) => {
+  res.send('create task')
+}
+
+const getTask = (req, res) => {
+  res.send('get single task')
+}
+
+const updateTask = (req, res) => {
+  res.send('update task')
+}
+
+const deleteTask = (req, res) => {
+  res.send('delete task')
+}
+
 module.exports = {
-  getAllTasks
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
 }
