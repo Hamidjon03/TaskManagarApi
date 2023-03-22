b7efdac4d604e82da266deca82af07b87c9dfac3
Hamidjon03 <forevalm@gmail.com>
2023-03-22 18:42:16 +0500

    Model added

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const connectStr = "mongodb://localhost:27017"
 
 const connectDB = (url) => {
-  return mongoose.connect(url, { useNewUrlParser: true }).then(() => console.log('meowDB'.rainbow))
+  return mongoose.connect(url, { useNewUrlParser: true }).then(() => console.log('meow'.cyan))
 }
 
 
