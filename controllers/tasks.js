6b9ada140da9c9b7ec725878ca4050a2ce20bc20
Hamidjon03 <forevalm@gmail.com>
2023-03-23 17:21:41 +0500

    get all tasks router created

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,16 +1,25 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req, res) => {
-  res.send('all items,items are getting by tasks controller')
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({})
+    res.status(200).json({ tasks })
+  } catch (error) {
+    res.status(500).json({ msg: err })
+  }
 }
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({task})
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task })
+  } catch (err) {
+    res.status(500).json({ msg: err })
+  }
 }
 
 const getTask = (req, res) => {
-  res.json({id: req.params.id})
+  res.json({ id: req.params.id })
 }
 
 const updateTask = (req, res) => {
