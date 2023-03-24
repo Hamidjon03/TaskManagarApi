20693ed170c96245ddf3d54c50e5461e6f7a6e9a
Hamidjon03 <forevalm@gmail.com>
2023-03-24 17:16:36 +0500

    notFound middleware added

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const connectDB = require('./db/connect')
 const app = express();
 const tasks = require('./routes/tasks')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 
 
 // middleware
+app.use(express.static('./public'))
 app.use(express.json())
 
 // routes
-app.get("/", (req, res) => {
-  res.send("Task manager is began")
-})
-
 app.use('/api/v1/tasks', tasks)
+
+app.use(notFound)
 
 const port = 3000;
 
